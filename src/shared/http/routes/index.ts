ab7230f7f7b112f20f86f1c3e2e2c2ca3b6c3365
Thiago <thiagolazaro2009@gmail.com>
2022-01-09 14:29:51 -0300

    Criado productRouter

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import productRouter from '@modules/products/routes/products.router';
 import { Router } from 'express';
 
 const routes = Router();
@@ -5,5 +6,7 @@ const routes = Router();
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
 });
+
+routes.use('/products', productRouter);
 
 export default routes;
