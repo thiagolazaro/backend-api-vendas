438606868a1b8775ee0659d0b9b557ba4fc2ec95
Thiago <thiagolazaro2009@gmail.com>
2022-01-26 22:50:31 -0300

    Adicionando modulo de middleware para express celebrate

--- src/modules/products/routes/products.router.ts
+++ src/modules/products/routes/products.router.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productRouter = Router();
 const productController = new ProductsController();
 
 productRouter.get('/', productController.index);
-productRouter.get('/:id', productController.show);
-productRouter.post('/', productController.create);
-productRouter.put('/:id', productController.update);
-productRouter.delete('/:id', productController.delete);
+
+productRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
+
+productRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number(),
+    },
+  }),
+  productController.create,
+);
+
+productRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+);
+
+productRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default productRouter;
